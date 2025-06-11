be99d649220711006754fdac5335393f4372b2aa
arthurzin157 <arthurzincosta54@gmail.com>
2025-06-11 10:34:42 -0300

    Add files via upload

---
+++ arthur.py
@@ -0,0 +1,19 @@
+nome = input("qual eo seu nome? ")
+temperatura = float(input("qual e a temperatura? "))
+
+
+if temperatura >= 20:
+   print(f"{nome} Quente")
+
+elif temperatura <=20:
+   print(f"{nome} frio")
+
+elif temperatura <=5:
+   print(f"{nome} gelado") 
+
+else:
+   print(f"{nome}maior que isso: noix morre")  
+
+
+
+
